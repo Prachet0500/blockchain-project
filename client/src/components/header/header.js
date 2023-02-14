27f7118a77b0f208bca54727cc0677cb85708e34
Prachet Salkade <247435@UST-INVLAB.COM>
2023-02-14 18:57:18 +0530

    implemented the header of the page

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,14 +1,28 @@
 import React from "react";
+import placeholder from  "../../images/placeholder.png"
 import { useNavigate } from "react-router";
 import './style.css'
 const Header =()=>{
+
     const navigate=useNavigate();
     const handleBuyClick=()=>{
-        navigate(',/buy')
+        navigate('/buy');
     }
+
     return(
 
-        <div className="Header"><button onClick={handleBuyClick}>Buy</button></div>
-    )
+        <div className="Header">
+            <div className="head">
+                <div className="hash">#Simple. #Transparent. #Secure</div><br/>
+                <div className="name">Extra Points Solutions</div><br/>
+                <div className="description">Products and services designed to help you reach your financial goals.</div><br/>
+                <button onClick={handleBuyClick}>Buy</button>
+            </div> 
+            <div>
+                <img src={placeholder} className="headerimg"alt='ok'></img>
+            </div>     
+        </div>
+    );
 }
+
 export default Header;
--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -2,28 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css'
  const NavBar=()=>{
- 
-    
+   
     return(
          <nav className="navbar">
-            <div className="title">
-                <h2> Secure Extra Points System </h2>
-            </div>
-            <div className="Menu">
+                <h2 className="title">
+                Secure Extra Points System 
+                </h2>
                 <ul className="list">
                     <li>
-                    <Link to='./earn-points'>Earn Points</Link>
+                    <Link style={{ 
+                        textDecoration: 'none' ,
+                        color:'black',fontFamily: 'Josefin Sans',fontStyle: 'normal',
+
+                        fontWeight: 400}}
+                        to='./earn-points'>Earn Points</Link>
                     </li>
                     <li>
-                    <Link to='./redeem'>Redeem Points</Link>
+                    <Link style={{ 
+                        textDecoration: 'none' ,
+                        color:'black',fontFamily: 'Josefin Sans',fontStyle: 'normal',
+                        fontWeight: 400 }}
+                        to='./redeem'>Redeem Points</Link>
                     </li>
-                    {/* <li>
-                    <Link to='./wallet'>Wallet</Link>
-                    </li> */}
+                    <li>
+                    <Link style={{ 
+                        textDecoration: 'none' ,
+                        color:'black',fontFamily: 'Josefin Sans',fontStyle: 'normal',
+                        fontWeight: 400 }}
+                        to='./wallet'>Wallet</Link>
+                    </li>
                 </ul>
-            </div>
         </nav> 
     );
 }
-//uuuu
 export default NavBar;
