418754d6e904515982ee0581ee93a549b11c990a
Prachet Salkade <247435@UST-INVLAB.COM>
2023-02-21 15:46:46 +0530

    made the buttons work

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './style.css';
+import { useNavigate } from 'react-router';
 
 
 function Card({ title, image, description }) {
+  const navigate=useNavigate();
+  const handleBuyClick=()=>{
+      navigate('/buy');
+  }
   console.log( title, image, description,'card')
   return (
     <div className="Card">
       <img className='card-img'src={image} alt='ok' />
       {/* <h2>{title}</h2> */}
       <p className='card-p'>{description}</p>
-      <button className='card-button'>Purchase</button>
+      <button className='card-button' onClick={handleBuyClick}>Purchase</button>
     </div>
   );
 }
